20a775f4e0fb4e321fed3ca54587aa686f937444
Barbara Bonati <barbarabonati@MacBook-Air-de-Barbara.local>
2025-07-25 17:28:26 -0400

    fix: remove duplicate Vite config

--- vite.config.js
+++ vite.config.js
@@ -7,9 +7,3 @@ export default defineConfig({
     outDir: 'dist'
   }
 })
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
-
-export default defineConfig({
-  plugins: [react()],
-});
